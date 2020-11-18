ead66c182ff1837d7e9db2c03b9d8f01d69627d5
Jacob Trueb <jtrueb@northwestern.edu>
2020-11-18 13:23:31 -0600

    Initial Commit

---
+++ QsibSensor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QsibSensor
+//
+//  Created by Jacob Trueb on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
