2ae97db7c58b0dbab769f7ba7a39461e63ad5bbc
Jacob Trueb <jtrueb@northwestern.edu>
2021-01-14 11:10:13 -0600

    Remove some dead copy paste settings

--- QsibSensor/StoreCallback.swift
+++ QsibSensor/StoreCallback.swift
@@ -17,23 +17,9 @@ public let STORE_CALLBACK: SubscriberCallback = StoreCallback()
 
 public class StoreCallback: SubscriberCallback {
     public func fire(_ state: inout QsibState, _ action: Action) {
-        LOGGER.trace("Fired SkinHydrationCallback on QsibState change for \(action)")
+        LOGGER.trace("Fired SubscriberCallback on QsibState change for \(action)")
         switch action {
         case _ as InitBle:
-            UserDefaults.standard.register(defaults: [
-                "rtg_upper_limit_value": 50,
-                "rtg_lower_limit_value": 20,
-
-                "ble_shs_enforce_strict_checks": true,
-                "ble_shs_ntc_bounds_lower": 30,
-                "ble_shs_ntc_bounds_upper": 80,
-                "ble_shs_ntc_diff": 24,
-                                
-                "ble_shs_name": ""
-            ])
-            
-    //        UserDefaults.standard.setValue("", forKey: "ble_shs_name")
-            
             // Begin UI refresh at least every N seconds
             UI_TICK()
         case _ as SetScan:
